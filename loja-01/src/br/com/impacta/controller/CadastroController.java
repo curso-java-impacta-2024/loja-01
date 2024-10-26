91d18180739fbdf276f7ebe456befb3a6806a6b9
alecarlosjesus <profalexandre.email@fiap.com.br>
2024-10-26 12:07:31 -0300

    Finalizando a listagem de dados com select all.

--- loja-01/src/br/com/impacta/controller/CadastroController.java
+++ loja-01/src/br/com/impacta/controller/CadastroController.java
@@ -1,6 +1,7 @@
 package br.com.impacta.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,50 +10,64 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.impacta.bean.FilmeBEAN;
+import br.com.impacta.bo.FilmeBO;
 
 /**
  * Servlet implementation class CadastroController
  */
-@WebServlet("/index.php")
+@WebServlet(urlPatterns= {"/index.php", "/listagem"})
+
 public class CadastroController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CadastroController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//Recebendo os dados do FORMULÁRIO E COLOCANDO NO OBJ...
-		FilmeBEAN fb = new FilmeBEAN();
-		//Pegando cada dado do formulário e adicionando no atributo do ObjFilme
-		fb.setTitulo( request.getParameter("titulo") );
-		fb.setDuracao( Integer.parseInt(request.getParameter("duracao")));
-		fb.setGenero(request.getParameter("genero"));
-		fb.setAnoLanc( Integer.parseInt(request.getParameter("anoLanc")));
-		
-		//Criando um atributo no request:
-		request.setAttribute("objFilme", fb);
-		
-		//Criando um dispatcher com o request para enviar o atributo
-		//para a nova página receber e ler o conteúdo...
-		request.getRequestDispatcher("result.jsp").forward(request, response);
-		
+	public CadastroController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String uriPath[] = request.getRequestURI().split("/");
+
+		if (uriPath[uriPath.length - 1].equals("index.php")) {
+			// Recebendo os dados do FORMULÁRIO E COLOCANDO NO OBJ...
+			FilmeBEAN fb = new FilmeBEAN();
+			// Pegando cada dado do formulário e adicionando no atributo do ObjFilme
+			fb.setTitulo(request.getParameter("titulo"));
+			fb.setDuracao(Integer.parseInt(request.getParameter("duracao")));
+			fb.setGenero(request.getParameter("genero"));
+			fb.setAnoLanc(Integer.parseInt(request.getParameter("anoLanc")));
+
+			// Criando um atributo no request:
+			request.setAttribute("objFilme", fb);
+
+			// Criando um dispatcher com o request para enviar o atributo
+			// para a nova página receber e ler o conteúdo...
+			request.getRequestDispatcher("result.jsp").forward(request, response);
+		}else if (uriPath[uriPath.length - 1].equals("listagem")) {
+			
+			//Instanciando a classe BO para iniciar o processo
+			// de listagem dos dados.
+			FilmeBO fbo = new FilmeBO();
+			//Recebendo a lista do método de listagem e colocando em uma lista
+			//do tipo FilmeBEAN
+			List<FilmeBEAN> lista =  fbo.listaFilme();
+						
+			// Criando um atributo no request:
+			request.setAttribute("listaDeFilmes", lista);
+			// Criando um dispatcher com o request para enviar o atributo com
+			// a lista para a página listagem receber e apresentar  o conteúdo...
+			request.getRequestDispatcher("listagem.jsp").forward(request, response);
+		}
 	}
 
+	
 }
