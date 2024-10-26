c698c45090043a99d9034d5f7acbc8cdd4c5ceac
MarianaGaldino <marianagaldino1@gmail.com>
2024-10-26 12:06:43 -0300

    adicionando pagina de listagem

--- loja-01/src/br/com/impacta/controller/CadastroController.java
+++ loja-01/src/br/com/impacta/controller/CadastroController.java
@@ -1,6 +1,7 @@
 package br.com.impacta.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import br.com.impacta.bean.FilmeBEAN;
+import br.com.impacta.bo.FilmeBO;
 
 /**
  * Servlet implementation class CadastroController
  */
-@WebServlet("/index.php")
+@WebServlet(urlPatterns= {"/index.php", "/listagem"})
 public class CadastroController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -30,22 +33,39 @@ public class CadastroController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//Recebendo os dados do FORMULÁRIO E COLOCANDO NO OBJ...
-		FilmeBEAN fb = new FilmeBEAN();
-		//Pegando cada dado do formulário e adicionando no atributo do ObjFilme
-		fb.setTitulo( request.getParameter("titulo") );
-		fb.setDuracao( Integer.parseInt(request.getParameter("duracao")));
-		fb.setGenero(request.getParameter("genero"));
-		fb.setAnoLanc( Integer.parseInt(request.getParameter("anoLanc")));
+		String uriPath[] = request.getRequestURI().split("/");
 		
-		//Criando um atributo no request:
-		request.setAttribute("objFilme", fb);
+		if(uriPath[uriPath.length -1].equals("index.php")) {
 		
-		//Criando um dispatcher com o request para enviar o atributo
-		//para a nova página receber e ler o conteúdo...
-		request.getRequestDispatcher("result.jsp").forward(request, response);
+			//Recebendo os dados do FORMULÁRIO E COLOCANDO NO OBJ...
+			FilmeBEAN fb = new FilmeBEAN();
+			//Pegando cada dado do formulário e adicionando no atributo do ObjFilme
+			fb.setTitulo( request.getParameter("titulo") );
+			fb.setDuracao( Integer.parseInt(request.getParameter("duracao")));
+			fb.setGenero(request.getParameter("genero"));
+			fb.setAnoLanc( Integer.parseInt(request.getParameter("anoLanc")));
+			
+			//Criando um atributo no request:
+			request.setAttribute("objFilme", fb);
+			
+			//Criando um dispatcher com o request para enviar o atributo
+			//para a nova página receber e ler o conteúdo...
+			request.getRequestDispatcher("result.jsp").forward(request, response);
 		
-	}
+		}else if(uriPath[uriPath.length - 1].equals("listagem")){
+			//instancia a classe bo para iniciar
+			FilmeBO fbo = new FilmeBO();
+			List<FilmeBEAN>listFilme = fbo.listaFilmes();
+			
+			//Criando um atributo no request:
+			request.setAttribute("listaDeFilmes", listFilme);
+			
+			//Criando um dispatcher com o request para enviar o atributo
+			//para a nova página receber e ler o conteúdo...
+			request.getRequestDispatcher("listagem.jsp").forward(request, response);
+			
+		}
+	}	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
