879615be8fe525fdb54dab332d387f0ebb03159c
GuiAz <guicottas@hotmail.com>
2024-10-26 10:33:49 -0300

    teste

--- loja-01/src/br/com/impacta/bo/FilmeBO.java
+++ loja-01/src/br/com/impacta/bo/FilmeBO.java
@@ -7,7 +7,7 @@ import br.com.impacta.dao.FilmeDAO;
 
 public class FilmeBO {
 
-	private  FilmeDAO fdao = null;
+	private  FilmeDAO fdao;
 	
 	public List<FilmeBEAN> listaFilme(){
 		fdao = new FilmeDAO();
